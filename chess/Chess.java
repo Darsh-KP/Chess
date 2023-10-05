86a6331e9af1a515f022fccfdb3da2411ca12c8c
DarshKP <133917099+Darsh-KP@users.noreply.github.com>
2023-10-05 17:40:30 -0400

    Strating pawn pieces
    Co-authored-by: opatel22 <opatel22@users.noreply.github.com>

--- chess/Chess.java
+++ chess/Chess.java
@@ -12,16 +12,13 @@ class ReturnPiece {
 	PieceType pieceType;
 	PieceFile pieceFile;
 	int pieceRank;  // 1..8
-
 	public String toString() {
 		return ""+pieceFile+pieceRank+":"+pieceType;
 	}
-
 	public boolean equals(Object other) {
 		if (other == null || !(other instanceof ReturnPiece)) {
 			return false;
 		}
-
 		ReturnPiece otherPiece = (ReturnPiece)other;
 		return pieceType == otherPiece.pieceType &&
 				pieceFile == otherPiece.pieceFile &&
@@ -41,37 +38,60 @@ class ReturnPlay {
 
 public class Chess {
 	// Playe enum to select between current player
-	enum Player { white, black }
-	
-	// chessBoard is stoing the pieces and the status of the game
-	private static ReturnPlay currentStatus = new ReturnPlay();
-	
+	enum Player {
+		white, black
+	}
+
+	// Stores the pieces and the status of the game
+	private static ReturnPlay currentStatus;
+	private static ReturnPiece[][] currentBoard;
+
 	public static ReturnPlay play(String move) {
 
 		return currentStatus; // Needs to return a ReturnPlay Object
 	}
-	
+
 	// Starts a new game of chess, puting all the pieces in their starting positions
 	public static void start() {
+		// Creates new return play object to reset game status
+		currentStatus = new ReturnPlay();
 		currentStatus.piecesOnBoard = new ArrayList<ReturnPiece>();
-		ArrayList<ReturnPiece> currentBoard = currentStatus.piecesOnBoard;
-		
-		currentBoard.add(new Pawn(Player.white, PieceFile.a, 2));
-		currentBoard.add(new Pawn(Player.white, PieceFile.b, 2));
-		currentBoard.add(new Pawn(Player.white, PieceFile.c, 2));
-		currentBoard.add(new Pawn(Player.white, PieceFile.d, 2));
-		currentBoard.add(new Pawn(Player.white, PieceFile.e, 2));
-		currentBoard.add(new Pawn(Player.white, PieceFile.f, 2));
-		currentBoard.add(new Pawn(Player.white, PieceFile.g, 2));
-		currentBoard.add(new Pawn(Player.white, PieceFile.h, 2));
 
-		currentBoard.add(new Pawn(Player.black, PieceFile.a, 7));
-		currentBoard.add(new Pawn(Player.black, PieceFile.b, 7));
-		currentBoard.add(new Pawn(Player.black, PieceFile.c, 7));
-		currentBoard.add(new Pawn(Player.black, PieceFile.d, 7));
-		currentBoard.add(new Pawn(Player.black, PieceFile.e, 7));
-		currentBoard.add(new Pawn(Player.black, PieceFile.f, 7));
-		currentBoard.add(new Pawn(Player.black, PieceFile.g, 7));
-		currentBoard.add(new Pawn(Player.black, PieceFile.h, 7));
+		// Creates new board
+		currentBoard = new ReturnPiece[8][8];
+
+		// Adds all the white pawns
+		for (int file = 0; file < 8; file++) currentBoard[getRank(2)][file] = new Pawn(Player.white, getFile(file), 2);
+
+		// Adds all the black pawns
+		for (int file = 0; file < 8; file++) currentBoard[getRank(7)][file] = new Pawn(Player.black, getFile(file), 7);
+
+		// Adds all the pieces from array to arraylist
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if (currentBoard[i][j] != null)
+					currentStatus.piecesOnBoard.add(currentBoard[i][j]);
+			}
+		}
+	}
+
+	// Helps gets the rank in the array, as the rank in the array starts from the top
+	public static int getRank(int rank) {
+		return 7 - rank;
+	}
+
+	// Converts a int for file to a PieceFile enum
+	public static PieceFile getFile(int file) {
+		switch (file) {
+            case 0: return PieceFile.a;
+            case 1: return PieceFile.b;
+            case 2: return PieceFile.c;
+            case 3: return PieceFile.d;
+            case 4: return PieceFile.e;
+            case 5: return PieceFile.f;
+            case 6: return PieceFile.g;
+            case 7: return PieceFile.h;
+        }
+		return PieceFile.a;
 	}
 }
--- chess/Pawn.java
+++ chess/Pawn.java
@@ -2,8 +2,9 @@ package chess;
 
 import chess.Chess.Player;
 
-public class Pawn extends ReturnPiece{
-    Pawn(Chess.Player player, ReturnPiece.PieceFile file, int rank) {
+public class Pawn extends ReturnPiece {
+    // Creates a pawn and which can be casted to returnPiece
+    Pawn(Chess.Player player, PieceFile file, int rank) {
         pieceType = (player == Player.white) ? PieceType.WP : PieceType.BP;
         pieceFile = file;
         pieceRank = rank;
